31b8e079a7f333898ec429125ca35a60f94887e0
wyd <wyd@rowenatech.com>
2018-08-27 19:13:22 +0800

    天假代码。

--- YDKit/YDSwiftKit/YDKit/YDBuilderSQL/Framework/DB/DB/YDSQLiteDB.swift
+++ YDKit/YDSwiftKit/YDKit/YDBuilderSQL/Framework/DB/DB/YDSQLiteDB.swift
@@ -6,8 +6,56 @@
 //  Copyright © 2018年 王允顶. All rights reserved.
 //
 
+//import Foundation
+//#if os(iOS)
+//import UIKit
+//#else
+//import AppKit
+//#endif
+
 import UIKit
 
+let SQLITE_DATE = SQLITE_NULL + 1
+
+private let SQLITE_STATIC = unsafeBitCast(0, to: sqlite3_destructor_type.self)
+private let SQLITE_TRANSIENT = unsafeBitCast(-1, to: sqlite3_destructor_type.self)
+
+@objc(YDSQLiteDB)
 class YDSQLiteDB: NSObject {
 
+    var dbName: String = "YDOrmDB.db"
+    let QUEUE_LABEL = "YDSQLiteBD"
+    private var db: OpaquePointer? = nil
+    private var queue: DispatchQueue!
+    private let fmt = DateFormatter()
+    private var path: String!
+    
+    init(dbName: String) {
+        super.init()
+        self.dbName = dbName
+        //文件管理
+        let fm = FileManager.default
+        //DB文件目录路径
+        var docDir = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]
+        
+        #if os(OSX)
+        
+        let info = Bundle.main.infoDictionary!
+        let appName: String = info["CFBundleName"] as! String
+        docDir = (docDir as NSString).appendingPathComponent(appName)
+        if !fm.fileExists(atPath: docDir) {
+            do {
+                
+            } catch {
+                print("创建DB目录错误： \(docDir) on macOS")
+            }
+        }
+        
+        #endif
+        
+        
+    }
+    
+    
+    
 }
---
+++ YDKit/YDSwiftKit/YDKit/YDExtension/String-Extras.swift
@@ -0,0 +1,55 @@
+//
+//  String-Extras.swift
+//  YDKit
+//
+//  Created by 王允顶 on 2018/8/19.
+//  Copyright © 2018年 王允顶. All rights reserved.
+//
+
+#if os(iOS)
+import UIKit
+#else
+import AppKit
+#endif
+
+extension String {
+
+    func positionOf(sub: String) -> Int {
+        var pos = -1
+        if let range = range(of: sub) {
+            if !range.isEmpty {
+                pos = self.distance(from: startIndex, to: range.lowerBound)
+//                pos = characters.distance(from: startIndex, to: range.lowerBound)
+            }
+        }
+        return pos
+    }
+    
+    func subString(start: Int, length: Int = -1) -> String {
+        var len = length
+        if len == -1 {
+            len = self.count - start
+        }
+        let st = self.index(startIndex, offsetBy: start)
+        let en = self.index(st, offsetBy: len)
+        
+        let range = st ..< en
+        return substring(with: range)
+        
+    }
+    
+    func urlEncoded() -> String {
+        let res: NSString = CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, self as NSString, nil, "!*'();:@&=+$,/?%#[]" as CFString, CFStringConvertNSStringEncodingToEncoding(String.Encoding.utf8.rawValue))
+        return res as String
+    }
+    
+    func urlDecoded() -> String {
+        let res: NSString = CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, self as NSString, nil, "" as CFString, CFStringConvertNSStringEncodingToEncoding(String.Encoding.utf8.rawValue))
+        return res as String
+    }
+    
+    func range() -> Range<String.Index> {
+        return Range<String.Index>(startIndex ..< endIndex)
+    }
+    
+}
