58ba75291245079d62b40b35a7ffc2f44e63fe5d
wyd <wyd@rowenatech.com>
2018-08-28 18:55:16 +0800

    添加代码。

--- YDKit/YDSwiftKit/YDKit/YDBuilderSQL/Framework/DB/DB/YDSQLiteDB.swift
+++ YDKit/YDSwiftKit/YDKit/YDBuilderSQL/Framework/DB/DB/YDSQLiteDB.swift
@@ -30,6 +30,7 @@ class YDSQLiteDB: NSObject {
     private let fmt = DateFormatter()
     private var path: String!
     
+    //MARK: ---: 构造方法 :---
     init(dbName: String) {
         super.init()
         self.dbName = dbName
@@ -39,7 +40,6 @@ class YDSQLiteDB: NSObject {
         var docDir = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]
         
         #if os(OSX)
-        
         let info = Bundle.main.infoDictionary!
         let appName: String = info["CFBundleName"] as! String
         docDir = (docDir as NSString).appendingPathComponent(appName)
@@ -50,12 +50,89 @@ class YDSQLiteDB: NSObject {
                 print("创建DB目录错误： \(docDir) on macOS")
             }
         }
-        
         #endif
+        let path = (docDir as NSString).appendingPathComponent(self.dbName)
+        if !(fm.fileExists(atPath: path)) {
+            guard let rp = Bundle.main.resourcePath else { return }
+            let from = (rp as NSString).appendingPathComponent(self.dbName)
+            do {
+                try fm.copyItem(atPath: from, toPath: path)
+            } catch let error as NSError {
+                print("DM_SQLiteDB - failed to copy writable version of DB!")
+                print("Error - \(error.localizedDescription)")
+                fm.createFile(atPath: path, contents: nil, attributes: nil)
+            }
+        }
+        openDB(path: path)
+    }
+    
+    private init(path: String) {
+        super.init()
+        openDB(path: path)
+    }
+    // MARK: ---: 析构函数 :---
+    deinit {
+        closeDB()
+    }
+    
+    override var description: String {
+        return "YDSQLiteDB: \(path)"
+    }
+    
+    // MARK: ---: 类方法 :---
+    class func openRO(path: String) -> YDSQLiteDB {
+        let db = YDSQLiteDB(path: path)
+        return db
+    }
+    
+    // MARK: ---: 公共方法 :---
+    func dbDate(dt: Date) -> String {
+        return fmt.string(from:dt)
+    }
+    
+    func dbDateFromString(str: String, format: String = "") -> Date? {
+        let dtFormat = fmt.dateFormat
+        if !format.isEmpty {
+            fmt.dateFormat = format
+        }
+        let dt = fmt.date(from: str)
+        if !format.isEmpty {
+            fmt.dateFormat = dtFormat
+        }
+        return dt
+    }
+    
+    func execute(sql: String, parameters: [Any]? = nil) -> CInt {
+        var result: CInt = 0
+        queue.sync {
+            if let stmt = self.prepare(sql: sql, params: parameters) {
+                result = self.execute(stmt: stmt, sql: sql)
+            }
+        }
+        return result
+    }
+    
+    private func openDB(path: String) {
         
+    }
+    private func closeDB() {
         
     }
     
+    //MARK: ---: 私有方法 :---
+    private func prepare(sql: String, params: [Any]?) -> OpaquePointer? {
+        var stmt: OpaquePointer 
+        
+        return stmt
+    }
+    // 私有方法处理实际执行的SQL语句
+    private func execute(stmt: OpaquePointer, sql: String) -> CInt {
+        var result = sqlite3_step(stmt)
+        
+        return result
+    }
     
     
 }
+
+
